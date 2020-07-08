c3765a58c6a12c97dc881c03e1f9aa8cb3739124
Gabriel Seals <sealsgj@gmail.com>
2020-07-08 15:04:03 -0500

    Updates

---
+++ precache-manifest.0b908e60c095f5832166f1378c540d53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e9477252c7a5d99f8c23826573f3983",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "0bec81032eb0ecc95cd7",
+    "url": "/pokemon/static/css/main.cbaa82e6.chunk.css"
+  },
+  {
+    "revision": "af5aab4733df2a57b8c8",
+    "url": "/pokemon/static/js/2.e33b8ea3.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/pokemon/static/js/2.e33b8ea3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bec81032eb0ecc95cd7",
+    "url": "/pokemon/static/js/main.60d9514c.chunk.js"
+  },
+  {
+    "revision": "b931b09f9078cae05f94",
+    "url": "/pokemon/static/js/runtime-main.89aedc36.js"
+  },
+  {
+    "revision": "0bda2da2153a00edb2b67f0ac5103307",
+    "url": "/pokemon/static/media/pokemonlogo.svg.0bda2da2.png"
+  }
+]);
