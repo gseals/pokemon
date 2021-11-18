4236be58dbaf5d04b4541d92efec34a2554d7484
Gabriel Seals <52087296+gseals@users.noreply.github.com>
2021-11-18 10:47:42 -0600

    Update App.js

--- src/App/App.js
+++ src/App/App.js
@@ -7,7 +7,7 @@ import './App.css';
 
 function App() {
   const client = new ApolloClient({
-    uri: 'https://graphql-pokemon.now.sh/',
+    uri: 'https://graphql-pokemon2.vercel.app/',
   });
 
   return (
